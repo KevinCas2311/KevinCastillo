bc979b83c376f032a48b4c46143368d1e6cf392d
asus <asus@DESKTOP-M9U0LGE>
2025-04-24 22:03:09 -0500

    segundo cambio realizado

---
+++ src/kevin_castillo/proyecton1/Suma.java
@@ -0,0 +1,19 @@
+package kevin_castillo.proyectoN1;
+
+import java.util.Scanner;
+
+public class Suma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa el primer numero: ");
+        double numero1 = Double.parseDouble(scanner.nextLine());
+
+        System.out.print("Ingresa el segundo numero: ");
+        double numero2 = Double.parseDouble(scanner.nextLine());
+
+        double suma = numero1 + numero2;
+
+        System.out.println("La suma es: " + suma);
+    }
+}
